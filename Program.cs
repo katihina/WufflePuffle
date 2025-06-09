6be1af8927427fc197553c21cbdc60a7da4cba91
katihina <ognevuschka@gmail.com>
2025-06-09 17:26:30 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Как вас зовут?");
+        string name = Console.ReadLine();
+        Console.WriteLine($"Привет, {name}!");
+        
+        Console.WriteLine("\nНажмите любую клавишу для выхода...");
+        Console.ReadKey();
+    }
+}
